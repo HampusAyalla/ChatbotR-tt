3487b7bbec77e863cb614b6c1d58b4d240bd9215
HampusAyalla <hampus.melinosterlindh@skola.taby.se>
2026-01-14 21:06:55 +0100

    Html, koppla html

--- app.js
+++ app.js
@@ -12,5 +12,14 @@ const db = mysql.createConnection({
     database: "chatbot"
 });
 
+db.connect(err => {
+    if (err) throw err;
+});
+
+// html
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
 app.listen(3000, () => {
 });
