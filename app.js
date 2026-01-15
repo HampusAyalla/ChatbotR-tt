d5ee5e0c14edfb2b090a9a8c740be3e5e65d359f
HampusAyalla <hampus.melinosterlindh@skola.taby.se>
2026-01-15 12:27:48 +0100

    jobbat

--- app.js
+++ app.js
@@ -21,5 +21,26 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/chat", (req, res) => {
+    const userInput = req.body.message;
+
+    const sql = "SELECT output FROM messages WHERE input = ?";
+    db.query(sql, [userInput], (err, result) => {
+        if (err) throw err;
+
+        let answer = "Du snackar gojja grabben";
+
+        if (result.length > 0) {
+            answer = result[0].output;
+        }
+
+        res.send(`
+            <p><b>Du:</b> ${userInput}</p>
+            <p><b>Chatbottis3000 :</b> ${answer}</p>
+            <a href="/">Tillbaka</a>
+        `);
+    });
+});
+
 app.listen(3000, () => {
 });
